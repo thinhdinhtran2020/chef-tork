4511d18dac5be6f88ec4d2aded118ab90014729c
dernah <144572300+dernah@users.noreply.github.com>
2023-11-09 01:03:34 -0500

    Enemy collision DMG Cooldown

--- Chef Tork/Assets/scripts/EnemyDamage.cs
+++ Chef Tork/Assets/scripts/EnemyDamage.cs
@@ -6,10 +6,13 @@ public class SpikeDamage : MonoBehaviour
 {
     [SerializeField] private float damage;
     public int Respawn;
+    private float damageCooldown = 2.0f;
+    private float lastDamageTime = -2.0f;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" && Time.time >= lastDamageTime + damageCooldown)
         {
+            lastDamageTime = Time.time;
             collision.GetComponent<Health>().TakeDamage(damage);
             //StartCoroutine(DelayedRespawn()); do a check later to only execute this if health == 0
         }
