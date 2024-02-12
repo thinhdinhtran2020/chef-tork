878ba36d3d7aaa1526d3d0ca112d7cb731b1ca3a
dernah <144572300+dernah@users.noreply.github.com>
2024-02-11 19:54:09 -0500

    Options Menu

--- Chef Tork/Assets/scripts/OptionsMenu.cs
+++ Chef Tork/Assets/scripts/OptionsMenu.cs
@@ -2,14 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
     public GameObject MainMenuUI;
     public GameObject optionMenuUI;
-    public void SetVolume (float volume)
-    {
 
+    public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        List<TMP_Dropdown.OptionData> dropdownOptions = new List<TMP_Dropdown.OptionData>();
+        foreach (string option in options)
+        {
+            TMP_Dropdown.OptionData newOption = new TMP_Dropdown.OptionData(option);
+            dropdownOptions.Add(newOption);
+        }
+
+        resolutionDropdown.AddOptions(dropdownOptions);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
     public void SaveOptions()
     {
